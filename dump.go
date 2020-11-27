7a2758c7c1073324b1a3fe032765c0f05f95b9a0
gaoqilin <334136724@qq.com>
2020-11-27 19:22:01 +0800

    add time.Now() , add caller find and func

--- dump.go
+++ dump.go
@@ -2,6 +2,9 @@ package dump
 
 import (
 	"fmt"
+	"runtime"
+	"strings"
+	"time"
 
 	"github.com/Jeffail/gabs/v2"
 	"github.com/fatih/color"
@@ -26,21 +29,44 @@ func ColorDump(c *color.Color, vals ...interface{}) {
 		Dump(vals...)
 		return
 	}
-	for _, v := range vals {
-		c.Println(JsonDump(v))
-	}
+	commonPrintln(c.Println, vals...)
 }
 
 // Dump
 // The same as PHP:var_dump()
 // https://www.php.net/var_dump
 func Dump(vals ...interface{}) {
-	for _, v := range vals {
-		fmt.Println(JsonDump(v))
-	}
+	commonPrintln(fmt.Println, vals...)
 }
 
 // JsonDump
 func JsonDump(v interface{}) string {
 	return gabs.Wrap(v).StringIndent("", "   ")
+}
+
+type F func(...interface{}) (int, error)
+
+func commonPrintln(f F, vals ...interface{}) {
+	tnow := timeNow()
+	file := debugTrace(3)
+	for _, v := range vals {
+		f(tnow, file, JsonDump(v))
+	}
+}
+
+func timeNow() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func debugTrace(i int) string {
+	funcName := ""
+	pc, file, line, _ := runtime.Caller(i)
+	fc := runtime.FuncForPC(pc)
+	if fc != nil {
+		funcName = fc.Name()
+		if pos := strings.LastIndex(funcName, "/"); pos > 0 {
+			funcName = funcName[pos+1:]
+		}
+	}
+	return fmt.Sprintf("%s:%d:%s()\n", file, line, funcName)
 }
